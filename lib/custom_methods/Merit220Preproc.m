716bfd44bd016836e04f88c3664ea315178d91eb
Erik Kastman <erik@nelson.medicine.wisc.edu>
2010-05-20 18:23:13 -0500

    Add Merit220 Preproc M-files

---
+++ lib/custom_methods/Merit220Preproc.m
@@ -0,0 +1,28 @@
+function [] = merit_preproc(studypath, images, image_boldreps, job_mfile)
+% [] = merit_preproc Calls and runs Merit220 preproc job.
+% merit_preproc('/private/tmp/mrt00015_orig/', ...
+% {'amrt00015_task1.nii' 'amrt00015_task2.nii' 'amrt00015_rest.nii'}, ...
+% {164 164 164}, ...
+% '/private/tmp/mrt00015_orig/mrt00015_preproc_job.m') 
+% 
+
+% List of open inputs
+% Realign: Estimate: Session - cfg_files
+% Realign: Estimate: Session - cfg_files
+% Realign: Estimate: Session - cfg_files
+nrun = 1; % enter the number of runs here
+jobfile = {job_mfile}
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(3, nrun);
+
+for crun = 1:nrun
+    for index = 1:length(images)
+        vols = {};
+        for vol = 1:image_boldreps{index}
+            vols{vol} = strcat(studypath, images{index}, ',', int2str(vol))
+        end
+        inputs{index, crun} = vols % Realign: Estimate: Session - cfg_files
+    end
+end
+spm('defaults', 'FMRI');
+spm_jobman('serial', jobs, '', inputs{:});
